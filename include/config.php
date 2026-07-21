dc7bcbceb0b4767164a30df43054c163eb5c2164
sandiris15 <mamangwaifi@gmail.com>
2026-07-21 11:01:57 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,7 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+
+
+$data['aramda'] = array ('1'=>'aramda!163.61.244.246:4522','aramda@|@sandi','aramda#|#cGk=','aramda%armada','aramda^armada','aramda&Rp','aramda*10','aramda(1','aramda)','aramda=10','aramda@!@enable');
